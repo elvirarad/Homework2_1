ad1d5ab34d1e13e0efe61bd75e7ba37230d9f1af
Elvira <rostechno_rb@mail.ru>
2022-12-21 10:37:54 +0500

    completed task 3 corrected 2

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -2,7 +2,7 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        Car ladaGranta = new Car("Lado","Granta", "Россия", -1.7F, 2015);
+        Car ladaGranta = new Car("Lada","Granta", "Россия", 1.7F, 2015);
         ladaGranta.color = "жёлтый";
         ladaGranta.print();
 
@@ -11,7 +11,7 @@ public class Main {
         audiA8.year = 2020;
         audiA8.print();
 
-        Car bmwZ8 = new Car("BMW", "Z85", "Германи", 3F);
+        Car bmwZ8 = new Car("BMW", "Z8", "Германия", 3F);
         bmwZ8.color = "черный";
         bmwZ8.year = 2021;
         bmwZ8.print();
@@ -20,7 +20,7 @@ public class Main {
         kiaSportage.color = "красный";
         kiaSportage.print();
 
-        Car hyundaiAvante = new Car("Hyundai", "Avante", "Южная Корея", 0F, 2016);
+        Car hyundaiAvante = new Car("Hyundai", "Avante", "Южная Корея", 01.6F, 2016);
         hyundaiAvante.print();
 
     }
